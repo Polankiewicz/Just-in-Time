8167c51b7a89a45bb97b5d141c26adaaace7f36c
Polankiewicz <damianpol2@o2.pl>
2015-03-31 10:41:41 +0200

    test 2

--- WindowsGame1/WindowsGame1/WindowsGame1/Building.cs
+++ WindowsGame1/WindowsGame1/WindowsGame1/Building.cs
@@ -49,6 +49,7 @@ namespace WindowsGame1
                     effect.Projection = camera.Projection;
                 }
                 mesh.Draw();
+                // hehe test
             }
 
         }
