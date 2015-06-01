d3700a7341afbcc444aa3b29ba26f89740312f84
Polankiewicz <damianpol2@o2.pl>
2015-06-02 01:55:27 +0200

    shooting prototype, only on Z vector from player

---
+++ WindowsGame1/WindowsGame1/WindowsGame1/Bullet.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WindowsGame1
+{
+    public class Bullet
+    {
+        float radius;
+        float distance;
+        float speed;
+        BoundingSphere _boundingSphere;
+
+        public BoundingSphere boundingSphere
+        {
+            get { return _boundingSphere; }
+            set { _boundingSphere = value; }
+        }
+
+
+        public Bullet(Vector3 center)
+        {
+            radius = 0.1f;
+            speed = 5f;
+            distance = 50f;
+
+            _boundingSphere = new BoundingSphere(center, radius);
+        }
+
+        public bool checkIfBulletIsTooFarAway(Vector3 cameraPosition)
+        {
+            if (Vector3.Distance(_boundingSphere.Center, cameraPosition) > distance)
+                return true;
+
+            return false;
+        }
+
+    }
+}
